eac17a239d78d117713bb734a6e3cb42f82f4376
Bruna Santos <brunasantos.ctt@gmail.com>
2024-01-26 17:01:51 -0300

    Comecando a implementar a impressao de tabuleiro

---
+++ JogoXadrez/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            pecas = new Peca[Linhas, Colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+    }
+}
