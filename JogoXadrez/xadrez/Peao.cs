2aab6ac8c2e5340aff233533580fd04b57a95dff
Bruna Santos <brunasantos.ctt@gmail.com>
2024-02-03 14:07:13 -0300

    commitando o jogo completo!

--- JogoXadrez/xadrez/Peao.cs
+++ JogoXadrez/xadrez/Peao.cs
@@ -5,9 +5,14 @@ namespace xadrez
 
     class Peao : Peca
     {
+        private PartidaDeXadrez partida;
 
         public Peao(Tabuleiro tab, Cor cor) : base(tab, cor)
         {
+        }
+        public Peao(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
+        {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -31,7 +36,7 @@ namespace xadrez
             bool[,] mat = new bool[tab.linhas, tab.colunas];
             Posicao pos = new Posicao(0, 0);
 
-            // Peoes Brancos
+            // Peões Brancos
             if (cor == Cor.Branca)
             {
                 pos.definirValores(posicao.linha - 1, posicao.coluna);
@@ -55,9 +60,24 @@ namespace xadrez
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
-            }
 
-            // Peoes Amarelos
+                //Jogada Especial - En Passant (Peões brancos)
+                if (posicao.linha == 3)
+                {
+                    Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulnuravelEnPassant)
+                    {
+                        mat[esquerda.linha - 1, esquerda.coluna] = true;
+                    }
+                    Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
+                    if (tab.posicaoValida(direita) && existeInimigo(direita) && tab.peca(direita) == partida.vulnuravelEnPassant)
+                    {
+                        mat[direita.linha - 1, direita.coluna] = true;
+                    }
+                }
+            }            
+
+            // Peões Amarelos
             else
             {
                 pos.definirValores(posicao.linha + 1, posicao.coluna);
@@ -80,6 +100,21 @@ namespace xadrez
                 if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
+                }
+
+                //Jogada Especial - En Passant (Peões brancos)
+                if (posicao.linha == 4)
+                {
+                    Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulnuravelEnPassant)
+                    {
+                        mat[esquerda.linha + 1, esquerda.coluna] = true;
+                    }
+                    Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
+                    if (tab.posicaoValida(direita) && existeInimigo(direita) && tab.peca(direita) == partida.vulnuravelEnPassant)
+                    {
+                        mat[direita.linha + 1, direita.coluna] = true;
+                    }
                 }
             }
 
